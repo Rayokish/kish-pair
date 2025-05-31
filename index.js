c6f121bbef07b1da0b0c8f6f89713560fb182963
Rayokish <159958022+Rayokish@users.noreply.github.com>
2025-05-31 16:54:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
+
 const PORT = process.env.PORT || 8000;
-let code = require('./pair');
+const __path = process.cwd();
+
+const pairRouter = require('./pair');
+const qrRouter = require('./qr');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/code', pairRouter); // Your pairing logic routes
+app.use('/qr', qrRouter);     // QR code pairing display
+
+// Serve HTML files
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__path, 'pair.html'));
+});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__path, 'main.html'));
+});
+
+// Start server
 app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star
+  console.log(' Welcome to Kish Pairing Server! ');
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
- Kish Pairing Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+module.exports = app;
